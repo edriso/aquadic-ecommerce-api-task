d00177b8e2c3c3986c942d55475473f4a7aa4e76
edriso <edriso.dev@gmail.com>
2024-02-02 10:15:41 +0200

    make migrations

--- database/migrations/2024_02_02_075137_create_products_table.php
+++ database/migrations/2024_02_02_075137_create_products_table.php
@@ -10,15 +10,10 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->enum('role', ['admin', 'client'])->default('client');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
